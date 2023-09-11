1fa4d41b37c1f9b45fc5bb9604f7cd81ed9239db
BelalAhmed4 <bello.elhagrasy@gmail.com>
2023-09-12 00:09:28 +0300

    update

---
+++ sideBarList/js/script.js
@@ -0,0 +1,20 @@
+// for search bar
+var input = document.getElementById('search-input');
+
+input.addEventListener("keyup", function () {
+  let searchBar = document.querySelector(".search-input").value.toUpperCase();
+  let productList = document.querySelector(".product-list");
+  let product = document.querySelectorAll(".product");
+  let productName = document.getElementsByTagName('h4');
+
+  for (let x = 0; x < productName.length; x++) {
+
+    if (productName[x].innerHTML.toUpperCase().indexOf(searchBar) >= 0) {
+
+      product[x].style.display = "";
+    } else {
+      product[x].style.display = "none"
+    }
+  };
+})
+// End for search bar
